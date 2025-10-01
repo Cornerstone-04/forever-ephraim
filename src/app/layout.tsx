96e02165e0e75cd6a37e42ac1a8d624f870d8f75
Cornerstone <fortunecornerstone@gmail.com>
2025-10-01 22:03:09 +0100

    feat: meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,33 @@ export const metadata: Metadata = {
       "I’m Forever, and I turn boring brand messages into stuff people actually want to read. If your brand needs words with personality, nice to meet you!",
     images: ["/forever-2.jpeg"],
   },
+  robots: "index, follow",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-32x32.png",
+    apple: "/apple-touch-icon.png",
+  },
+  keywords: [
+    "Forever Ephraim",
+    "Creative Copywriter",
+    "Content Writer",
+    "Email Copywriter",
+    "Brand Storyteller",
+    "Freelancer",
+    "Copywriting Services",
+    "Marketing Copywriter",
+    "SEO Copywriter",
+    "Social Media Copywriter",
+    "Website Copywriter",
+    "Product Descriptions",
+    "Blog Writing",
+    "Content Strategy",
+    "Brand Messaging",
+    "Advertising Copywriter",
+    "Digital Marketing",
+    "Freelance Writer",
+    "Copywriting Portfolio",
+  ],
 };
 
 export default function RootLayout({
